1a0b188312b8637243d12df61119da988e330784
I Gusti Kade S <igks.learning@gmail.com>
2020-04-03 17:06:06 +0700

    add auth guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,19 +1,27 @@
 import { Injectable } from "@angular/core";
 import { CanActivate, Router } from "@angular/router";
 import { AuthService } from "app/services/auth.service";
+import { AlertService } from "app/services/alert.service";
 
 @Injectable({
     providedIn: "root"
 })
 export class AuthGuard implements CanActivate {
-    constructor(private authService: AuthService, private router: Router) {}
+    constructor(
+        private authService: AuthService,
+        private router: Router,
+        private alert: AlertService
+    ) {}
 
     canActivate(): boolean {
         if (this.authService.isAuthenticated()) {
             return true;
         }
 
-        alert("not authenticated");
+        this.alert.Error(
+            "You are not authenticated",
+            "Please login to the application"
+        );
         this.router.navigate(["/auth/login"]);
         return false;
     }
